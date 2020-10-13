17f9ee45b7d6bf50462be4c16c4d2bdf38e252e3
Andrey Karetnikov <andrey.karetnikov@gmail.com>
2020-10-14 00:10:02 +0300

    feat: add solution for Morse Decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = [];
+
+    for (let j = 0; j < expr.length; j = j + 10) {
+        let newString = [];
+
+        for (let i = 0; i < 9; i = i + 2) {
+            if (expr[j + i] === '1' && expr[j + i + 1] === '0') {
+                newString.push('.');
+            } else if (expr[j + i] === '1' && expr[j + i + 1] === '1') {
+                newString.push('-');
+            } else if (expr[j + i] === "*") {
+                newString.push(' ');
+            } 
+        }
+
+        result.push(MORSE_TABLE[newString.join('')]);
+
+    }
+
+    for (let w = 0; w < result.length; w++) {
+      if (result[w] === undefined) result[w] = ' ';
+    }
+
+    return result.join('');
+
 }
 
 module.exports = {
